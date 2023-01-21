61e27c1019d7447471affd310bde94a50d0f9a84
githumbi <githumbi74@gmail.com>
2023-01-21 11:30:03 +0300

    initialize

---
+++ src/components/cakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeContainer = () => {
+  return (
+    <div>
+      <h2>number of cakes</h2>
+      <button>click</button>
+    </div>
+  )
+}
+
+export default CakeContainer
